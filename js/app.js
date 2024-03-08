046bed0a5f8512d664e6155633777a365caaf734
Hasinur3813 <hasinur.rahman1332@gmail.com>
2024-03-08 21:27:44 +0600

    update

--- js/app.js
+++ js/app.js
@@ -4,23 +4,28 @@ const board = document.getElementById("grid");
 const scoreBoard = document.querySelector(".current_score");
 const highScore = document.querySelector(".high_score");
 const gameOverBoard = document.querySelector(".game_over_board");
-const gameOverText = document.querySelector(".game_over_board h2");
-const game_over = new Audio('./audio/game_over.mp3');
-const snake_eating = new Audio('./audio/snake_eating.mp3');
-const arrowButtons = document.querySelectorAll('.arrowBtns');
+const gameOverText = document.querySelector(".gameOverInfo h2");
+const gameOverInfo = document.querySelector(".gameOverInfo");
+const game_over = new Audio("./audio/game_over.mp3");
+const snake_eating = new Audio("./audio/snake_eating.mp3");
+const arrowButtons = document.querySelectorAll(".arrowBtns");
+const rangeSpeed = document.getElementById("vol");
+const playBtn = document.getElementById("play_btn");
+const view_speed = document.getElementById("view_speed");
+const speed_controller = document.getElementById("speed_controller");
 
 let currentSnake = [2, 1, 0];
 let direction = 1;
 let width = 30;
-let speed = 5;
-let is_game_over = false;
+let speed = 4;
 let foodIndex = 0;
 let score = 0;
+let speedValue = 2;
 let interval;
 
 for (let i = 0; i < 900; i++) {
   let cell = document.createElement("div");
-  cell.classList.add('single_grid');
+  cell.classList.add("single_grid");
   board.appendChild(cell);
 }
 
@@ -30,25 +35,24 @@ cells[currentSnake[0]].classList.add("head");
 for (let i = 0; i < currentSnake.length; i++) {
   cells[currentSnake[i]].classList.add("snake");
 }
-interval = setInterval(gameLoop, speed * 20);
 
 function gameLoop() {
   let is_hits_border_or_body = chekForHits();
 
   if (is_hits_border_or_body) {
-    is_game_over = true;
     gameOver();
   } else {
-    is_game_over = false;
     moveSnake();
   }
 }
 window.addEventListener("DOMContentLoaded", moveSnakeByKey);
 
 function moveSnakeByKey() {
+  rangeSpeed.value = 2;
   highScore.innerHTML = `High score:  ${localStorage.getItem("score")}`;
-
-  window.addEventListener('keydown', function (keys){changeDirection(keys.key)})
+  window.addEventListener("keydown", function (keys) {
+    changeDirection(keys.key);
+  });
 }
 
 function moveSnake() {
@@ -57,7 +61,6 @@ function moveSnake() {
   currentSnake.unshift(currentSnake[0] + direction);
   cells[currentSnake[1]].classList.remove("head");
   cells[currentSnake[0]].classList.add("snake", "head");
-
   eatFood(tail);
 }
 
@@ -70,20 +73,21 @@ function gameOver() {
   game_over.play();
   clearInterval(interval);
   pushGameOverBox();
-
 }
 
-function pushGameOverBox(){
-  gameOverBoard.style.display = 'flex';
-  setTimeout(()=>{
-    gameOverText.style.transform = 'scale(1.2)';
-  },100)
-
+function pushGameOverBox() {
+  gameOverBoard.style.display = "flex";
+  speed_controller.style.display = 'none';
+  gameOverInfo.style.display = 'flex';
+  setTimeout(() => {
+    gameOverText.style.transform = "scale(1.2)";
+  }, 100);
 }
 
 function eatFood(tail) {
   if (currentSnake[0] == foodIndex) {
     cells[foodIndex].classList.remove("food");
+    cells[foodIndex].innerHTML = '';
     snake_eating.play();
     placeFood();
     increaseSnake(tail);
@@ -91,7 +95,6 @@ function eatFood(tail) {
 }
 
 function increaseSnake(tail) {
-  cells[tail].classList.add("food");
   currentSnake.push(tail);
   cells[tail].classList.remove("food");
   countScore();
@@ -112,11 +115,12 @@ function placeFood() {
   }
 
   cells[randomFood].classList.add("food");
+  cells[randomFood].innerHTML = '&#127813';
   foodIndex = randomFood;
 }
 
 function makeRandomFood() {
-  let randomNumber = Math.floor(Math.random() * (900 - 0)) + 0;
+  let randomNumber = Math.floor(Math.random() * 900) + 0;
   return randomNumber;
 }
 
@@ -138,12 +142,14 @@ function chekForHits() {
   }
 }
 
-arrowButtons.forEach(function(btns){
-  btns.addEventListener('click',function(){changeDirection(btns.classList[0])});
+arrowButtons.forEach(function (btns) {
+  btns.addEventListener("click", function () {
+    changeDirection(btns.classList[0]);
+  });
 });
 
-function changeDirection(key){
-  switch(key) {
+function changeDirection(key) {
+  switch (key) {
     case "ArrowRight":
       direction = direction != -1 ? 1 : -1;
       break;
@@ -158,3 +164,16 @@ function changeDirection(key){
       break;
   }
 }
+
+rangeSpeed.addEventListener('input',function(){
+  speedValue = rangeSpeed.value;
+  view_speed.innerHTML = 'Set Speed:' + ' ' + speedValue;
+})
+
+playBtn.addEventListener('click', function(){
+  speed = speedValue;
+  gameOverBoard.style.display = 'none';
+  interval = setInterval(gameLoop, speed * 50);
+
+})
+
